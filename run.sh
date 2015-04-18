5eba9489de30d64e006265d501043b9fbe384deb
Sébastien Cevey <seb.cevey@guardian.co.uk>
2015-04-18 16:22:37 +0100

    Fix ftp-watcher flag and name

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ x-terminal-emulator -T elasticsearch -e "cd elasticsearch; ./dev-start.sh" &
 x-terminal-emulator -T media-api     -e "sbt 'project media-api'    'run 9001'" &
 x-terminal-emulator -T thrall        -e "sbt 'project thrall'       'run 9002'" &
 x-terminal-emulator -T image-loader  -e "sbt 'project image-loader' 'run 9003'" &
-x-terminal-emulator -T ftp-loader    -e "sbt -Dactive=$FTP_ACTIVE
+x-terminal-emulator -T ftp-watcher   -e "sbt -Dftp.active=$FTP_ACTIVE
                                              'project ftp-watcher'  'run 9004'" &
 x-terminal-emulator -T kahuna        -e "sbt 'project kahuna'       'run 9005'" &
 x-terminal-emulator -T cropper       -e "sbt 'project cropper'      'run 9006'" &
