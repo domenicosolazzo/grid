8e202dbb6dc24cdef60184b69972f2fc10c7e477
Akash A <akash1810@gmail.com>
2015-08-07 11:26:39 +0100

    Updating new resources to a more accurate name.

--- thrall/app/lib/Config.scala
+++ thrall/app/lib/Config.scala
@@ -38,5 +38,5 @@ object Config extends CommonPlayAppConfig {
 
   val healthyMessageRate = properties("sqs.message.min.frequency").toInt
 
-  val imageUploadTopicArn: String = properties("image.upload.sns.topic.arn")
+  val imageIndexedTopicArn: String = properties("image.indexed.sns.topic.arn")
 }
